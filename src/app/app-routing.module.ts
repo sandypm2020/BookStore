5f6bd9b32006d9854deb011cb292d50bde5348f3
snehalp29 <snehalpatkar@gmail.com>
2020-09-02 13:59:09 -0400

    Added Shell Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  {
+    path: 'books',
+    component: BookListComponent,
+  },
+  {
+    path: 'users',
+    component: UserListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
